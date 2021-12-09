6ffd61bdbbdb164becd1bb0523e73c3e947334bf
suraj9982 <surajsk9982@gmail.com>
2021-12-09 17:22:15 +0530

    [suraj] add: This is basic third party libraries

---
+++ ThirdPartyLibraries/ThirdPartyLibraries/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ThirdPartyLibraries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // CSVHandler.ImplementCSVHandler();
+            ReadCsvAndWriteToWrite.ImplementCSVToJsonHandler();
+        }
+    }
+}
---
+++ ThirdPartyLibraries/ThirdPartyLibraries/ReadCsvAndWriteToWrite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using Newtonsoft.Json;
+
+namespace ThirdPartyLibraries
+{
+    class ReadCsvAndWriteToWrite
+    {
+        public static void ImplementCSVToJsonHandler()
+        {
+            string ImportFilepath = @"C:\Users\admin\Desktop\github\ThirdPartyLibraries\ThirdPartyLibraries\Files\JSonData.csv";
+            string ExportFilepath = @"C:\Users\admin\Desktop\github\ThirdPartyLibraries\ThirdPartyLibraries\Files\Export.Json";
+            using (var reader = new StreamReader(ImportFilepath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddresssData>().ToList();
+                Console.WriteLine("read data successfully from address.csv");
+                foreach (AddresssData address in records)
+                {
+                    Console.WriteLine("\t" + address.firstname + "\t" + address.lastname + "\t" + address.address + "\t" + address.city + "\t" + address.state + "\t" + address.code);
+                    Console.WriteLine("\n");
+                }
+                Console.WriteLine("***********Now reading from csv file and write to csv file**********");
+                JsonSerializer serializer = new JsonSerializer();
+                using (StreamWriter sr = new StreamWriter(ExportFilepath))
+                using (JsonWriter writer = new JsonTextWriter(sr))
+                {
+                    serializer.Serialize(writer, records);
+                }
+            }
+        }
+    }
+}
